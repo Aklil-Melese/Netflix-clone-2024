b3a2c87781f90b5ff14d99060eefb4589a719c34
Your Name <>
2024-09-16 00:44:59 +0300

    rows

--- netflix-clone/src/components/Footer/Footer.jsx
+++ netflix-clone/src/components/Footer/Footer.jsx
@@ -5,18 +5,18 @@ import YouTubeIcon from "@mui/icons-material/YouTube";
 import "../Footer/footer.css";
 const Footer = () => {
   return (
-    <div className="footer-outer-container">
-      <div className="footer-inner-container">
-        <div className="footer-icons">
+    <div className='footer_outer_container'>
+      <div className='footer_inner_container'>
+        <div className='footer_icons'>
           <FacebookOutlinedIcon />
           <InstagramIcon />
           <YouTubeIcon />
-        </div> 
-        <div className="footer-data">
+        </div>
+        <div className='footer_data'>
           <div>
             <ul>
-              <li>Audio DEscription</li>
-              <li>Invester Relation</li>
+              <li>Audio Description</li>
+              <li>Investor Relations</li>
               <li>Legal Notice</li>
             </ul>
           </div>
@@ -30,25 +30,30 @@ const Footer = () => {
           <div>
             <ul>
               <li>Gift Cards</li>
-              <li>Terms of use</li>
+              <li>Terms of Use</li>
               <li>Corporate Information</li>
             </ul>
           </div>
           <div>
             <ul>
-              <li>Media Cente</li>
+              <li>Media Center</li>
               <li>Privacy</li>
-              <li>Contact us</li>
+              <li>Contact Us</li>
             </ul>
           </div>
-          <div className="Service-code">
-            <p>Service code</p>
-          </div>
-          <div className="Copy-right">&copy; 1997-2024 Netflix,Inc.</div>
+        </div>
+        <div className='service_code'>
+          <p>
+            Service Code
+          </p>
+        </div>
+        <div className='copy-write'>
+          &copy; 1997-2024 Netflix, Inc.
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
+
