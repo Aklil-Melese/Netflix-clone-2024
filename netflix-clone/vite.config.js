e2fee7fc3da670e51f3fc0abf3c3740d885ac512
Your Name <>
2024-12-06 15:17:06 +0300

    vite edition

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Netflix-clone-2024/netflix-clone/'
-})
+  base: "Netflix-clone-2024",
+});
